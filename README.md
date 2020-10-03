9634a6452c21f9006aa3ad17fa8445bf3a0a843f
JayWasTaken <jaymay8k@gmail.com>
2020-10-02 21:04:12 -0700

    backwards print works

---
+++ README.md
@@ -0,0 +1,6 @@
+#Poetry Slam
+
+In the spirit of Halloween, as it soon approaches, this program will take the poem "Haunted Houses" by Henry Wadsworth Longfellow and randomize the lines. Nothing is more scary than something you cant understand, after all.
+
+There is a .txt file with each line of the poem written in order. 
+There will be 3 functions, one prints the poem backwards, one prints it randomly, and one prints it with every two lines switched.
--- poetry_slam.py
+++ poetry_slam.py
@@ -1,14 +1,47 @@
-def foo(jj)
-    num_lines = len(jj)
-    jj = jj[::-1] # reverses the list
-    # 1 -> num_lines
-    for i in range(len(jj)):
-        #print(num_lines - 1)
-        print(jj[i])
-        variable = str(num_lines - i) + " " + jj[i]
-        print(variable)
+import random
 
-f = open('./poem.txt', 'r') # open a file
+def get_file_lines(filename):
+    poem_file = "/Users/jay/courses/cs1.0/poetry_slam/poem.txt"
+    infile = open(poem_file, 'r').readlines
+    infile.close()
+    return infile
+#string of the filename
+#return list of strings containing lines of the file
 
-#foo([1, 2, 3, 4])
-foo(f.read().splitlines()) #pass the list of lines to foo
+#-------Backwards Lines-------#
+def lines_printed_backwards(lines_list):
+    lines_list = open("/Users/jay/courses/cs1.0/poetry_slam/poem.txt", "r").readlines()
+    lines_list = lines_list[:: -1]
+    num_lines = len(lines_list)
+    for line in range(len(lines_list)):
+        reverse = str(num_lines - line) + " " + lines_list[line]
+        print(reverse) #print lines in reverse, include original line number
+#-------Random Lines---------#
+    
+        #random lines, can repeat, just have same number of lines as original. 
+
+#-------Custom Lines---------#
+# def lines_printed_custom(lines_list):
+#     lines_list = "/Users/jay/courses/cs1.0/poetry_slam/poem.txt"
+
+#     print() #some unique way, switch it so instead of 1234 its 2143? somethin
+print(lines_printed_backwards('poem.txt'))
+
+
+
+
+
+# def foo(jj)
+#     num_lines = len(jj)
+#     jj = jj[::-1] # reverses the list
+#     # 1 -> num_lines
+#     for i in range(len(jj)):
+#         #print(num_lines - 1)
+#         print(jj[i])
+#         variable = str(num_lines - i) + " " + jj[i]
+#         print(variable)
+
+# f = open('./poem.txt', 'r') # open a file
+
+# #foo([1, 2, 3, 4])
+# foo(f.read().splitlines()) #pass the list of lines to foo
